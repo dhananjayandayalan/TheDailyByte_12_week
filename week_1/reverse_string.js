05311763118de7e2b6323a2c9846e4207c76402b
Dhananjayan D <dhananjayan.dayalan@gmail.com>
2023-05-29 16:36:39 +0530

    1. Reverse String using Javascript array functions

--- week_1/reverse_string.js
+++ week_1/reverse_string.js
@@ -1,3 +1,11 @@
 const reverseString = (str) => {
-
+  const stringArray = str.split(' ');
+  const reverseString = stringArray.reduce((finalStr, str) => {
+    return finalStr + str.split('').reverse().join('') + ' ';
+  }, '');
+  return reverseString.trim();
 };
+
+console.log(reverseString('Cat'));
+console.log(reverseString('The Daily Byte'));
+console.log(reverseString('civic'));
